f4ba2bb3e2243608791940908c570ba9353c4cfe
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2023-06-27 15:23:31 -0700

    Added eslint.js and fixed errors

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-const hamburgertoggle = document.querySelector("#hamburger-toggle");
-const navbar = document.querySelector("#menulist");
-const menu = document.querySelector("#menuicon");
-const close = document.querySelector("#closeicon")
-const logo = document.querySelector(".mylogo");
-const navitems = document.querySelectorAll(".navlink");
-const nav = document.querySelector(".nav-container")
+/* eslint-disable linebreak-style */
+// CONSTVARIABLES
+const hamburgertoggle = document.querySelector('#hamburger-toggle');
+const navbar = document.querySelector('#menulist');
+const menu = document.querySelector('#menuicon');
+const close = document.querySelector('#closeicon');
+const logo = document.querySelector('.mylogo');
+const navitems = document.querySelectorAll('.navlink');
 
+function onhamburgerclick() {
+  // hamburgertoggle.classList.toggle('open')
+  menu.classList.add('hidden');
+  logo.classList.add('offlogo');
 
-
-hamburgertoggle.addEventListener("click", onhamburgerclick)
-
-function onhamburgerclick(){
-    // hamburgertoggle.classList.toggle('open')
-    menu.classList.add('hidden');
-    logo.classList.add('offlogo');
-   
-    
-    if(!navbar.classList.contains("open")){
-        navbar.classList.add("open");  
-        close.classList.add('hide'); 
-    }
-    else {
-        navbar.classList.remove("open");
-        menu.classList.remove('hidden');
-        logo.classList.remove('offlogo');
-        navbar.classList.remove('exit')
-        close.classList.remove('hide');
-    }
-
-   
-};
+  if (!navbar.classList.contains('open')) {
+    navbar.classList.add('open');
+    close.classList.add('hide');
+  } else if (navbar.classList.contains('open')) {
+    navbar.classList.remove('open');
+    menu.classList.remove('hidden');
+    logo.classList.remove('offlogo');
+    navbar.classList.remove('exit');
+    close.classList.remove('hide');
+  }
+}
+hamburgertoggle.addEventListener('click', onhamburgerclick);
 navitems.forEach((item) => {
-    
-    item.addEventListener('click', () =>{
-        navbar.classList.add('exit');
-        menu.classList.remove('hidden');
-        logo.classList.remove('offlogo');
-        close.classList.remove('hide');
-        
-    });
+  item.addEventListener('click', () => {
+    navbar.classList.add('exit');
+    menu.classList.remove('hidden');
+    logo.classList.remove('offlogo');
+    close.classList.remove('hide');
+  });
 });
