bed31f4635727d65f3c4578757fae3c783b84c75
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2023-06-27 09:09:56 -0700

    Update Style.css and script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const hamburgertoggle = document.querySelector("#hamburger-toggle");
 const navbar = document.querySelector("#menulist");
+const menu = document.querySelector("#menuicon");
+const logo = document.querySelector(".mylogo");
 
 hamburgertoggle.addEventListener("click", onhamburgerclick)
 
 function onhamburgerclick(){
+    hamburgertoggle.classList.toggle('open')
+    menu.classList.add('hidden');
+    logo.classList.add('offlogo')
     if(!navbar.classList.contains("open")){
         navbar.classList.add("open");
+        
+        
     }
     else {
         navbar.classList.remove("open");
+        menu.classList.remove('hidden')
+        logo.classList.remove('offlogo')
     }
 }
