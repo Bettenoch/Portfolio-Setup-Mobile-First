1d250ff6bba44ba9aebcd1fa4cb50c12cb7e9b97
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2023-06-27 14:36:14 -0700

    Update style.css and script.js

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 const hamburgertoggle = document.querySelector("#hamburger-toggle");
 const navbar = document.querySelector("#menulist");
 const menu = document.querySelector("#menuicon");
+const close = document.querySelector("#closeicon")
 const logo = document.querySelector(".mylogo");
+const navitems = document.querySelectorAll(".navlink");
+const nav = document.querySelector(".nav-container")
+
+
 
 hamburgertoggle.addEventListener("click", onhamburgerclick)
 
 function onhamburgerclick(){
-    hamburgertoggle.classList.toggle('open')
+    // hamburgertoggle.classList.toggle('open')
     menu.classList.add('hidden');
-    logo.classList.add('offlogo')
+    logo.classList.add('offlogo');
+   
+    
     if(!navbar.classList.contains("open")){
-        navbar.classList.add("open");
-        
-        
+        navbar.classList.add("open");  
+        close.classList.add('hide'); 
     }
     else {
         navbar.classList.remove("open");
-        menu.classList.remove('hidden')
-        logo.classList.remove('offlogo')
+        menu.classList.remove('hidden');
+        logo.classList.remove('offlogo');
+        navbar.classList.remove('exit')
+        close.classList.remove('hide');
     }
-}
+
+   
+};
+navitems.forEach((item) => {
+    
+    item.addEventListener('click', () =>{
+        navbar.classList.add('exit');
+        menu.classList.remove('hidden');
+        logo.classList.remove('offlogo');
+        close.classList.remove('hide');
+        
+    });
+});
