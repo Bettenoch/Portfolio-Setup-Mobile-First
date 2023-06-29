c4fdcd4791d2e980c1bb655e3b7dd55cb4f0153b
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2023-06-29 12:53:48 -0700

    Removed eslint and stylelint errors

--- script.js
+++ script.js
@@ -33,53 +33,17 @@ navitems.forEach((item) => {
 
 // FORM VALIDATION;
 
-// const errorMessage = document.querySelector('#form-validity');
 const userNameE = document.querySelector('#myName');
 const form = document.querySelector('#myForm');
 const emailAdress = document.querySelector('#myEmail');
-// const errorPopup = 'Your Email Should be in Lower Case';
 
-const checkUsername = () => {
-
-  let valid = false;
-
-  const min = 3,
-      max = 25;
-
-  const username = userNameE.value.trim();
-
-  if (!isRequired(username)) {
-      showError(userNameE, 'Username cannot be blank.');
-  } else if (!isBetween(username.length, min, max)) {
-      showError(userNameE, `Username must be between ${min} and ${max} characters.`)
-  } else {
-      showSuccess(userNameE);
-      valid = true;
-  }
-  return valid;
-};
-
-const checkEmail = () => {
-  let valid = false;
-  const email = emailAdress.value.trim();
-  if (!isRequired(email)) {
-      showError(emailAdress, 'Email cannot be blank.');
-  } else if (!isEmailValid(email)) {
-      showError(emailAdress, 'Email is not valid.')
-  } else {
-      showSuccess(emailAdress);
-      valid = true;
-  }
-  return valid;
-};
+const isRequired = (value) => (value !== '');
+const isBetween = (length, min, max) => (!(length < min || length > max));
 
 const isEmailValid = (email) => {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 };
-
-const isRequired = value => value === '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
 const showError = (input, message) => {
   // get the form-field element
@@ -104,49 +68,81 @@ const showSuccess = (input) => {
   // hide the error message
   const error = formField.querySelector('small');
   error.textContent = '';
-}
+};
 
-form.addEventListener('submit', function (e) {
+const checkUsername = () => {
+  let valid = false;
+
+  const min = 3;
+  const max = 25;
+
+  const username = userNameE.value.trim();
+
+  if (!isRequired(username)) {
+    showError(userNameE, 'Username cannot be blank.');
+  } else if (!isBetween(username.length, min, max)) {
+    showError(userNameE, `Username must be between ${min} and ${max} characters.`);
+  } else {
+    showSuccess(userNameE);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkEmail = () => {
+  let valid = false;
+  const email = emailAdress.value.trim();
+  if (!isRequired(email)) {
+    showError(emailAdress, 'Email cannot be blank.');
+  } else if (!isEmailValid(email)) {
+    showError(emailAdress, 'Email is not valid.');
+  } else {
+    showSuccess(emailAdress);
+    valid = true;
+  }
+  return valid;
+};
+
+form.addEventListener('submit', (e) => {
   // prevent the form from submitting
   e.preventDefault();
 
   // validate fields
-  let isUsernameValid = checkUsername(),
-      isEmailValid = checkEmail();
-      
-  let isFormValid = isUsernameValid &&
-      isEmailValid;
-    
-  // submit to the server if the form is valid
-  if (isFormValid) {
+  const isUsernameValid = checkUsername();
+  const isEmailValid = checkEmail();
 
+  const isFormValid = isUsernameValid
+      && isEmailValid;
+
+  if (isFormValid) {
+    // submit to the server if the form is valid
   }
 });
 
 const debounce = (fn, delay = 500) => {
   let timeoutId;
   return (...args) => {
-      // cancel the previous timer
-      if (timeoutId) {
-          clearTimeout(timeoutId);
-      }
-      // setup a new timer
-      timeoutId = setTimeout(() => {
-          fn.apply(null, args)
-      }, delay);
+    // cancel the previous timer
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    // setup a new timer
+    timeoutId = setTimeout(() => {
+      // eslint-disable-next-line prefer-spread
+      fn.apply(null, args);
+    }, delay);
   };
 };
 
-form.addEventListener('input', debounce(function (e) {
+form.addEventListener('input', debounce((e) => {
   switch (e.target.id) {
-      case 'myName':
-          checkUsername();
-          break;
-      case 'myEmail':
-          checkEmail();
-          break;
+    case 'myName':
+      checkUsername();
+      break;
+    case 'myEmail':
+      checkEmail();
+      break;
+    default:
+    // do nothing
   }
 }));
-
-
-
